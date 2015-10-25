e703f652bfac811d67f88caad4d63530027b03b6
Aidan Bickford <aidan.bickford@gmail.com>
2015-10-25 16:36:17 -0600

    Add mySQL to config

--- Vagrantfile
+++ Vagrantfile
@@ -13,6 +13,7 @@ Vagrant.configure(2) do |config|
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
   config.vm.box = "precise64"
+  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
@@ -54,18 +55,14 @@ Vagrant.configure(2) do |config|
   # View the documentation for the provider you are using for more
   # information on available options.
 
-  # Define a Vagrant Push strategy for pushing to Atlas. Other push strategies
-  # such as FTP and Heroku are also available. See the documentation at
-  # https://docs.vagrantup.com/v2/push/atlas.html for more information.
-  # config.push.define "atlas" do |push|
-  #   push.app = "YOUR_ATLAS_USERNAME/YOUR_APPLICATION_NAME"
-  # end
-
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
+
+
   config.vm.provision :shell, path:"bootstrap.sh"
 
   config.vm.network :forwarded_port, guest: 5000, host:4567
+  config.vm.network :forwarded_port, guest: 2200, host:2200
 
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,38 @@ cd /vagrant/presidential/
 echo "creating virtual env"
 virtualenv flask
 
+echo "installing mySQL"
+#http://www.thisprogrammingthing.com/2013/getting-started-with-vagrant/
+#http://www.sitepoint.com/vagrantfile-explained-setting-provisioning-shell/
+#http://stackoverflow.com/questions/23959457/connect-to-mysql-on-vagrant-instance-with-sequel-pro
+#https://github.com/AlexDisler/mysql-vagrant/blob/master/install.sh
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
+echo "preparing install"
+sudo apt-get -y install mysql-server
+
+# http://stackoverflow.com/questions/24855942/vagrant-ssh-setting-up-and-connecting-to-mysql
+# fill in the blanks for root password, db name, username (local), and password
+mysql -u root -p"vagrant" -e "CREATE DATABASE presidential_db;
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
+CREATE USER 'root'@'%' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'root'@'%';
+FLUSH PRIVILEGES;"
+
+# change bind address to allow connections from anywhere
+sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
+
+echo "restarting mySQL"
+
+sudo service mysql restart
+
 echo "installing libs"
 
-echo 
+echo
 flask/bin/pip install flask
 flask/bin/pip install flask-login
 flask/bin/pip install flask-openid
@@ -26,4 +55,3 @@ flask/bin/pip install flask-babel
 flask/bin/pip install guess_language
 flask/bin/pip install flipflop
 flask/bin/pip install coverage
-
